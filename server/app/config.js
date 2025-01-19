56a44347ffc6ecda00afb7d19ceab32862af8175
Irene Sánchez <enerisan@gmail.com>
2025-01-19 19:40:50 +0100

    fix: fixing cors

--- server/app/config.js
+++ server/app/config.js
@@ -8,7 +8,6 @@ const app = express();
 
 /* ************************************************************************* */
 
-
 // CORS Handling: Why is the current code commented out and do I need to define specific allowed origins for my project?
 
 // CORS (Cross-Origin Resource Sharing) is a security mechanism in web browsers that blocks requests from a different domain than the server.
@@ -28,20 +27,20 @@ const app = express();
 
 const cors = require("cors");
 
-/* app.use(
+app.use(
   cors({
     origin: [
       process.env.CLIENT_URL, // keep this one, after checking the value in `server/.env`
     ],
     credentials: true,
-  })  
-); */
-app.use(
+  })
+);
+/* app.use(
   cors({
     origin: "*",  // Permite todas las conexiones, solo para pruebas
     credentials: true,
   })
-);
+); */
 
 /* ************************************************************************* */
 
