ca1e5980236e9a207c8ea00eec1ad028642b5d29
Irene Sánchez <enerisan@gmail.com>
2025-01-19 19:56:41 +0100

    fix: fixing cors problem

--- server/app/config.js
+++ server/app/config.js
@@ -27,12 +27,19 @@ const app = express();
 
 const cors = require("cors");
 
-app.use(
+/* app.use(
   cors({
     origin: [
       process.env.CLIENT_URL, // keep this one, after checking the value in `server/.env`
     ],
     credentials: true,
+  })
+); */
+
+app.use(
+  cors({
+    origin: "http://eatingnamnam.enerisan.com", // Cambia esto por tu dominio real.
+    credentials: true, // Si necesitas cookies o autenticación
   })
 );
 /* app.use(
